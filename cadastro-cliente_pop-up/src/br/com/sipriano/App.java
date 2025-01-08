99dbd8d0033f00cd83083241b41872d8f64c1eb5
Anderson Sipriano <Andsipriano@gmail.com>
2025-01-08 16:07:55 -0300

    Organiação e melhoria da validação do cadastro de dados

--- cadastro-cliente_pop-up/src/br/com/sipriano/App.java
+++ cadastro-cliente_pop-up/src/br/com/sipriano/App.java
@@ -34,26 +34,26 @@ public class App {
 
                 cadastrar(obterDados());
 
-            } else if(isConsultar(opcao)) {
+            } else if (isConsultar(opcao)) {
                 String dados = JOptionPane.showInputDialog(null,
                         "Digite o cpf",
                         "Consultar", JOptionPane.INFORMATION_MESSAGE);
 
                 consultar(dados);
 
-            } else if(isExcluir(opcao)) {
+            } else if (isExcluir(opcao)) {
                 String cpf = JOptionPane.showInputDialog(null,
                         "Digite o Cpf do cliente o qual quer excluir: ",
                         "Exclusão", JOptionPane.INFORMATION_MESSAGE);
                 excluir(cpf);
 
-            } else if(isAlterar(opcao)) {
+            } else if (isAlterar(opcao)) {
                 String cpf = JOptionPane.showInputDialog(null,
                         "Digite o cpf do usuário o qual quer alterar: ",
                         "Alterar", JOptionPane.INFORMATION_MESSAGE);
                 alterar(cpf);
 
-            } else if(isBuscarTodos(opcao)) {
+            } else if (isBuscarTodos(opcao)) {
                 buscarTodos();
             }
 
@@ -64,15 +64,14 @@ public class App {
     }
 
 
-    private static void cadastrar(String dados) {
-        String[] dadosSeparados = criarArray(dados);
+    private static void cadastrar(String[] dadosSeparados) {
 
         Cliente cliente = instanciaCliente(dadosSeparados);
         Boolean isCadastrado = iClienteDAO.cadastrar(cliente);
         if (isCadastrado) {
-            JOptionPane.showMessageDialog(null, "Cliente cadastrado com sucesso ", "Sucesso",JOptionPane.INFORMATION_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Cliente cadastrado com sucesso ", "Sucesso", JOptionPane.INFORMATION_MESSAGE);
         } else {
-            JOptionPane.showMessageDialog(null, "Cliente já se encontra cadastrado", "Erro",JOptionPane.INFORMATION_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Cliente já se encontra cadastrado", "Erro", JOptionPane.INFORMATION_MESSAGE);
         }
     }
 
@@ -80,15 +79,15 @@ public class App {
         Long cpf = validarCPF(dados);
         while (cpf == null) {
             dados = JOptionPane.showInputDialog(null,
-            "Digite o cpf",
-            "Consultar", JOptionPane.INFORMATION_MESSAGE);
+                    "Digite o cpf",
+                    "Consultar", JOptionPane.INFORMATION_MESSAGE);
             cpf = validarCPF(dados);
         }
         Cliente cliente = iClienteDAO.consultar(cpf);
         if (cliente != null) {
-            JOptionPane.showMessageDialog(null, "Cliente encontrado: " + cliente.toString(), "Sucesso",JOptionPane.INFORMATION_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Cliente encontrado: " + cliente.toString(), "Sucesso", JOptionPane.INFORMATION_MESSAGE);
         } else {
-            JOptionPane.showMessageDialog(null, "Cliente não encontrado: ", "Sucesso",JOptionPane.INFORMATION_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Cliente não encontrado: ", "Fail", JOptionPane.INFORMATION_MESSAGE);
         }
 
 
@@ -98,7 +97,7 @@ public class App {
         Cliente cliente = iClienteDAO.consultar(Long.parseLong(cpf));
         if (cliente != null) {
             iClienteDAO.excluir(cliente.getCpf());
-            JOptionPane.showMessageDialog(null,"Cliente deletado", "Sucesso", JOptionPane.INFORMATION_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Cliente deletado", "Sucesso", JOptionPane.INFORMATION_MESSAGE);
         } else {
             JOptionPane.showMessageDialog(null, "Cliente não encontrado", "Falha", JOptionPane.INFORMATION_MESSAGE);
         }
@@ -107,9 +106,9 @@ public class App {
     private static void alterar(String cpf) {
         Cliente cliente = iClienteDAO.consultar(Long.parseLong(cpf));
         if (cliente != null) {
-            String[] dadosSeparados = criarArray(obterDados());
+            String[] dadosSeparados = obterDados();
             iClienteDAO.alterar(instanciaCliente(dadosSeparados));
-            JOptionPane.showMessageDialog(null,"Cliente alterado", "Sucesso", JOptionPane.INFORMATION_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Cliente alterado", "Sucesso", JOptionPane.INFORMATION_MESSAGE);
         } else {
             JOptionPane.showMessageDialog(null, "Cliente não encontrado", "Falha", JOptionPane.INFORMATION_MESSAGE);
         }
@@ -120,7 +119,7 @@ public class App {
     }
 
     private static void sair() {
-        JOptionPane.showMessageDialog(null, "Até logo: ", "Sair",JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, "Até logo: ", "Sair", JOptionPane.INFORMATION_MESSAGE);
         System.exit(0);
     }
 
@@ -175,11 +174,33 @@ public class App {
         return false;
     }
 
-    private static String obterDados() {
+    private static String[] obterDados() {
         String dados = JOptionPane.showInputDialog(null,
                 "Digite os dados do cliente separados por vígula, conforme exemplo: CPF, Nome, Telefone, Endereço, Número, Cidade, Estado",
                 "Cadastro", JOptionPane.INFORMATION_MESSAGE);
-        return dados;
+        String[] dadosSeparados = new String[7];
+        String[] temp = dados.split(",");
+        System.arraycopy(temp, 0, dadosSeparados, 0, temp.length);
+
+        while (!validarDados(dadosSeparados)) {
+            dados = JOptionPane.showInputDialog(null,
+                    "Dados faltando! Digite os dados do cliente separados por vígula, conforme exemplo: CPF, Nome, Telefone, Endereço, Número, Cidade, Estado",
+                    "Cadastro", JOptionPane.INFORMATION_MESSAGE);
+            dadosSeparados = new String[7];
+            temp = dados.split(",");
+            System.arraycopy(temp, 0, dadosSeparados, 0, temp.length);
+        }
+
+        return dadosSeparados;
+    }
+
+    private static boolean validarDados(String[] dadosSeparados) {
+        for (String campo : dadosSeparados) {
+            if (campo == null || "".equals(campo)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     private static Cliente instanciaCliente(String[] dadosSeparados) {
@@ -187,12 +208,6 @@ public class App {
         return new Cliente(dadosSeparados[0], dadosSeparados[1], dadosSeparados[2], dadosSeparados[3], dadosSeparados[4], dadosSeparados[5], dadosSeparados[6]);
     }
 
-    private static String[] criarArray(String dados) {
-        String[] dadosSeparados = new String[7];
-        String[] temp = dados.split(",");
-        System.arraycopy(temp, 0, dadosSeparados, 0, temp.length);
-        return dadosSeparados;
-    }
 
     private static Long validarCPF(String dados) {
         String cpf = dados.replaceAll("[^0-9]", ""); // remove caracteres não numéricos
